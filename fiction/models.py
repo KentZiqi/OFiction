0915d7e03c183a177280b5330ea4bda4850721b3
ZiqiXiong <zx012013@mymail.pomona.edu>
2015-09-26 23:07:23 -0700

    Add comment model

--- fiction/models.py
+++ fiction/models.py
@@ -13,6 +13,7 @@ class Profile(models.Model):
     display_name = models.CharField(blank=True,max_length=50)
     description = models.TextField(blank=True,null=True)
     stars = models.ManyToManyField(Chapter,related_name="likers")
+    subscription = models.ManyToManyField(Fiction,related_name="subscribers")
     def __str__(self):
         return self.display_name
 
@@ -27,6 +28,12 @@ class Chapter(models.Model):
     authors = models.ManyToManyField(Profile)
     created_date = models.DateTimeField(default=datetime.datetime.now)
     parent = models.ForeignKey(ChapterVersion)
+
+class Comment(models.Model):
+    commenter = models.ForeignKey(Profile)
+    chapter = models.ForeignKey(Chapter)
+    body = models.TextField()
+    created_date = models.DateTimeField(default=datetime.datetime.now)
 
 class Fiction(models.Model):
     title = models.CharField(max_length=200)
