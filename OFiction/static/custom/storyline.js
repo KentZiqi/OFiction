0648f4ae2d255995e78b7d7853c619fefe7139f9
theworldbright <kent@kentshikama.com>
2015-12-13 17:03:43 -0800

    Add initial front-end for storyline view

--- OFiction/static/custom/storyline.js
+++ OFiction/static/custom/storyline.js
@@ -1,58 +1,35 @@
-jsPlumb.ready(function () {
+$(function () {
+    var cy = window.cy = cytoscape({
+                                       container: document.getElementById('cy'),
 
-    var instance = jsPlumb.getInstance({Container: "canvas"});
-    instance.connect({
-                         source: "node-1",
-                         target: "node-2",
-                         paintStyle: {lineWidth: 4, strokeStyle: '#333'},
-                         connector: "Straight",
-                         anchors: ["Bottom", "Top"],
-                         endpoint: "Blank",
-                         endpointStyle: {fillStyle: "#333"}
-                     })
-    instance.connect({
-                         source: "node-2",
-                         target: "node-3",
-                         paintStyle: {lineWidth: 4, strokeStyle: '#333'},
-                         connector: "Straight",
-                         anchors: ["Bottom", "Top"],
-                         endpoint: "Blank",
-                         endpointStyle: {fillStyle: "#333"}
-                     })
-    instance.connect({
-                         source: "node-3",
-                         target: "node-4",
-                         paintStyle: {lineWidth: 4, strokeStyle: '#333'},
-                         connector: "Straight",
-                         anchors: ["Bottom", "Top"],
-                         endpoint: "Blank",
-                         endpointStyle: {fillStyle: "#333"}
-                     })
-    instance.connect({
-                         source: "node-3",
-                         target: "node-5",
-                         paintStyle: {lineWidth: 4, strokeStyle: '#333'},
-                         connector: "Straight",
-                         anchors: ["Bottom", "Top"],
-                         endpoint: "Blank",
-                         endpointStyle: {fillStyle: "#333"}
-                     })
-    instance.connect({
-                         source: "node-4",
-                         target: "node-6",
-                         paintStyle: {lineWidth: 4, strokeStyle: '#333'},
-                         connector: "Straight",
-                         anchors: ["Bottom", "Top"],
-                         endpoint: "Blank",
-                         endpointStyle: {fillStyle: "#333"}
-                     })
-    instance.connect({
-                         source: "node-5",
-                         target: "node-7",
-                         paintStyle: {lineWidth: 4, strokeStyle: '#333'},
-                         connector: "Straight",
-                         anchors: ["Bottom", "Top"],
-                         endpoint: "Blank",
-                         endpointStyle: {fillStyle: "#333"}
-                     })
+                                       boxSelectionEnabled: false,
+                                       autounselectify: true,
+
+                                       layout: {
+                                           name: 'dagre'
+                                       },
+
+                                       style: [
+                                           {
+                                               selector: 'node',
+                                               style: {
+                                                   'content': 'data(label)',
+                                                   'text-opacity': 0.5,
+                                                   'text-valign': 'center',
+                                                   'text-halign': 'center',
+                                                   'background-color': '#11479e'
+                                               }
+                                           },
+
+                                           {
+                                               selector: 'edge',
+                                               style: {
+                                                   'width': 4,
+                                                   'target-arrow-shape': 'triangle',
+                                                   'line-color': '#9dbaea',
+                                                   'target-arrow-color': '#9dbaea'
+                                               }
+                                           }
+                                       ]
+                                   });
 });
